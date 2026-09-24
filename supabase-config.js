57da4df5606ffa169b095d90db7170f7a971bfd1
summertweet <594247827@qq.com>
2026-09-24 21:04:06 +0800

    Connect reviews to Supabase cloud storage

---
+++ supabase-config.js
@@ -0,0 +1,11 @@
+window.SUPABASE_CONFIG = {
+  url: 'https://vgxqhetcetvbigijhwhf.supabase.co',
+  anonKey: 'sb_publishable_6jkm09PRrom6sevasfKH9Q_K6mTs0e8',
+  users: {
+    '高泽雄': 'gzx@ssgc-weekly-review.local',
+    '林少雄': 'lsx@ssgc-weekly-review.local',
+    '蔡灿嵘': 'ccr@ssgc-weekly-review.local',
+    '吴世友': 'wsy@ssgc-weekly-review.local',
+    '陈炳煌': 'cbh@ssgc-weekly-review.local'
+  }
+};
